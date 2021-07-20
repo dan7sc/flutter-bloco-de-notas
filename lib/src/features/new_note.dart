e28994528cc7255d8641f4f7210139a88888f924
dan7sc <su7cheng@gmail.com>
2021-07-20 09:11:28 -0300

    Add NewNotePage

---
+++ lib/src/features/new_note.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewNotePage extends StatefulWidget {
+  @override
+  _NewNotePageState createState() => _NewNotePageState();
+}
+
+class _NewNotePageState extends State<NewNotePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
