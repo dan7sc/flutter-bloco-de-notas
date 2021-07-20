b8a90db5ff007319e8178474cd7886d0713f33a6
dan7sc <su7cheng@gmail.com>
2021-07-20 13:11:32 -0300

    Put row with icons in the bottom

--- lib/src/features/new_note.dart
+++ lib/src/features/new_note.dart
@@ -158,45 +158,52 @@ class _NewNotePageState extends State<NewNotePage> {
             ),
           ),
           Visibility(
-            child: Card(
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.date_range,
-                      color: Colors.black.withOpacity(0.54),
-                    ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SafeArea(
+                bottom: true,
+                child: Card(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.date_range,
+                          color: Colors.black.withOpacity(0.54),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.attach_file_outlined,
+                          color: Colors.black.withOpacity(0.54),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.favorite_border,
+                          color: Colors.black.withOpacity(0.54),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.black.withOpacity(0.54),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.black.withOpacity(0.54),
+                        ),
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.attach_file_outlined,
-                      color: Colors.black.withOpacity(0.54),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.favorite_border,
-                      color: Colors.black.withOpacity(0.54),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.share,
-                      color: Colors.black.withOpacity(0.54),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.black.withOpacity(0.54),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
